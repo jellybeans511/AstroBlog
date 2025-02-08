15cc99fde0df18c629694bedce73da7fb40e2399
DocteuMan <name.jellybeans511>
2025-02-08 22:30:40 +0900

    AstroBlog first commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'てう速5センチメートル';
+export const SITE_DESCRIPTION = 'てう速5センチメートルの公式サイトです。';
